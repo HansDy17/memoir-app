3f8de1d70a01fca1b0a7155a51fef65a078c1f4b
Hans Benson L. Dy Tuan <hansbenson.dytuan@g.msuiit.edu.ph>
2025-05-27 12:49:25 +0800

    feat: add UserController and Users index page for user management

---
+++ memoir-app/app/Http/Controllers/UserController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        // Logic to retrieve and display users
+        return Inertia::render('Users/Index', [
+            
+        ]);
+    }
+
+    public function show($id)
+    {
+        // Logic to retrieve and display a specific user
+        return view('user.show', ['id' => $id]);
+    }
+
+    public function edit($id)
+    {
+        // Logic to edit a specific user
+        return view('user.edit', ['id' => $id]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        // Logic to update a specific user
+        // Validate and save the user data
+        return redirect()->route('user.show', ['id' => $id]);
+    }
+}
