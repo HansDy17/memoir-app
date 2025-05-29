7e163a1f5e5b584809cecfa490ca028757b9d941
Hans Benson L. Dy Tuan <hansbenson.dytuan@g.msuiit.edu.ph>
2025-05-29 10:50:16 +0800

    feat: update AppLogo component with custom font and title

--- memoir-app/resources/js/components/app-logo.tsx
+++ memoir-app/resources/js/components/app-logo.tsx
@@ -3,11 +3,23 @@ import AppLogoIcon from './app-logo-icon';
 export default function AppLogo() {
     return (
         <>
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
+            <link
+                href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&display=swap"
+                rel="stylesheet"
+            />
+            
             <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-md">
                 <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
-                <span className="mb-0.5 truncate leading-none font-semibold">Laravel Starter Kit</span>
+                <h1
+                    className="text-4xl md:text-3xl leading-tight dark:text-[#EDEDEC]"
+                    style={{ fontFamily: '"Dancing Script", cursive' }}
+                >
+                    Memoir
+                </h1>
             </div>
         </>
     );
